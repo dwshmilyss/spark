1c2896ff1071ae467b77c3632d226602fe4d7e9f
dwshmilyss <dwshmilyss@163.com>
2017-09-15 15:14:36 +0800

    my_1 up

--- examples/src/main/scala/org/apache/spark/examples/HdfsTest.scala
+++ examples/src/main/scala/org/apache/spark/examples/HdfsTest.scala
@@ -18,6 +18,7 @@
 // scalastyle:off println
 package org.apache.spark.examples
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
 
@@ -25,23 +26,29 @@ object HdfsTest {
 
   /** Usage: HdfsTest [file] */
   def main(args: Array[String]) {
-    if (args.length < 1) {
-      System.err.println("Usage: HdfsTest <file>")
-      System.exit(1)
-    }
-    val spark = SparkSession
-      .builder
-      .appName("HdfsTest")
-      .getOrCreate()
-    val file = spark.read.text(args(0)).rdd
-    val mapped = file.map(s => s.length).cache()
-    for (iter <- 1 to 10) {
-      val start = System.currentTimeMillis()
-      for (x <- mapped) { x + 2 }
-      val end = System.currentTimeMillis()
-      println("Iteration " + iter + " took " + (end-start) + " ms")
-    }
-    spark.stop()
+//    if (args.length < 1) {
+//      System.err.println("Usage: HdfsTest <file>")
+//      System.exit(1)
+//    }
+//    val spark = SparkSession
+//      .builder
+//      .appName("HdfsTest")
+//      .getOrCreate()
+//    val file = spark.read.text("hdfs://master01:9000/a.txt").rdd
+    val conf = new SparkConf()
+    conf.setMaster("local[*]").setAppName("HdfsTest")
+    val sc = new SparkContext(conf)
+    val file = sc.textFile("hdfs://master01:9000/a.txt")
+    println(file.count())
+//    val mapped = file.map(s => s.length).cache()
+//    for (iter <- 1 to 10) {
+//      val start = System.currentTimeMillis()
+//      for (x <- mapped) { x + 2 }
+//      val end = System.currentTimeMillis()
+//      println("Iteration " + iter + " took " + (end-start) + " ms")
+//    }
+//    spark.stop()
+    sc.stop()
   }
 }
 // scalastyle:on println
